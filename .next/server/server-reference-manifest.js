3b4710e4367a6cf0e75a8b91f9e3df0df36c5d09
codeftshaunak <sagardeyyash19@gmail.com>
2025-10-27 13:06:33 +0600

    remove unnecesarry file

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6XuNFEPG7EswWCdx5LfLByY9QKUQ/ZBo5QbHb2D4oWw=\"\n}"
--- .next/static/chunks/src_app_page_tsx_8f3504f5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_7e46bbb6._.js",
-  "static/chunks/node_modules_b918be05._.js"
-],
-    source: "dynamic"
-});
